38999dc6a1505ed5e6310e1eddc7f859b9ac1c20
Denis Titov <denis.titov.dua@gmail.com>
2023-11-06 02:06:18 +0600

    models

---
+++ app/Models/Feedback.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Feedback extends Model
+{
+    use HasFactory;
+
+    public $table = 'feedbacks';
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class, 'status_id', 'id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+
+
+    protected $fillable = ['user_id, category_id, theme, message, answer, file, status_id, created_at'];
+}
