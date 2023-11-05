e9aecfe9a0e1b77c334c082d791a9a7aa0b17e26
Denis Titov <denis.titov.dua@gmail.com>
2023-11-06 01:55:18 +0600

    first commit

---
+++ resources/js/Pages/Feedbacks/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FeedbackIndex(){
+    return(
+        <div>
+            Заявки
+        </div>
+    );
+}
---
+++ resources/js/app.jsx
@@ -0,0 +1,8 @@
+import './bootstrap';
+import React from 'react';
+
+import {render} from "react-dom"
+import FeedbackIndex from './Pages/Feedbacks';
+
+render(<FeedbackIndex/>, document.getElementById('app'))
+
